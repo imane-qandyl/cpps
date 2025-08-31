c8be7252e434a710ccaa8b1d4b5dfa93f5cf9dc4
Imane Qandyl <imqandyl@lab1r4s1.42abudhabi.ae>
2025-08-31 18:46:04 +0400

    main with more details

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,15 +6,36 @@
 /*   By: imqandyl <imqandyl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 20:01:10 by imqandyl          #+#    #+#             */
-/*   Updated: 2025/08/31 18:42:25 by imqandyl         ###   ########.fr       */
+/*   Updated: 2025/08/31 18:45:23 by imqandyl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
+
+class And {
+    public:
+        And( void ) : _a(0) {}
+        And(And const & src) { 
+            std::cout << "Copy constructor called" << std::endl;
+            *this = src; }
+        And &operator=( And const & rhs) { if (this != &rhs) { this->_a = rhs._a; } return *this; }
+        And( int a) : _a(a) {}
+        ~And( void ) {}
+        int getValue() const { return _a; }
+    private:
+        int _a;
+};
+    
 int main() 
 {
     std::cout << "----------------- Main 1 -----------------" << std::endl;
+    
+    And obj1(42), obj2(84);
+    swap(obj1, obj2);
+    std::cout << "obj1 = " << obj1.getValue()  << std::endl;
+    
+    std::cout << "----------------- Main 2 -----------------" << std::endl;
 	{
 		int a = 2;
 		int b = 3;
